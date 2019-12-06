1420538ff32040977d248a6496fd7224c6ff6a3f
Joseph Tatum <josephtatum@gmail.com>
2019-12-06 14:28:42 -0800

    wrote post artist endpoint and test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -10,4 +10,21 @@ describe('Each model', () => {
         expect(res.body).toEqual({ norskGreeting: 'Hei!' });
       });
   });
+
+  it('has a route that posts an object', () => {
+    return request(app)
+      .post('/artist')
+      .send({
+        name: 'Rosalia',
+        birthday: '08/02/1992',
+        origin: 'Spain'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          name: 'Rosalia',
+          birthday: '08/02/1992',
+          origin: 'Spain'
+        });
+      });
+  });
 });
